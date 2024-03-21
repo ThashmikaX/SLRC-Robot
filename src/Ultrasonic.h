74f9f6dc7a6449361a13fae6dbe145b99689a8dc
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-03-22 00:41:53 +0530

    Updated pin configurations for Ultrasonic and LED modules

--- src/Ultrasonic.h
+++ src/Ultrasonic.h
@@ -4,8 +4,8 @@
 const int trigPin = 25;
 const int echoPin = 27;
 
-const int trigPin2 = 23;
-const int echoPin2 = 22;
+const int trigPin2 = 43;
+const int echoPin2 = 42;
 // defines variables
 
 void forwardDistanceSetup();
--- src/led.h
+++ src/led.h
@@ -1,8 +1,8 @@
 #ifndef LED_H
 #define LED_H
 
-const int Blue = 13;
-const int Green = 12;
+const int Blue = 50;
+const int Green = 51;
 
 void setupLEDs();
 void onBlue();
